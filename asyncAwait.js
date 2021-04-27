8aae05ec286c16afbd95051d346e7f2b3dc8771b
dasha <misery.hatred.anger@gmail.com>
2021-04-27 11:09:16 +0300

    No return statement in async & right function names

--- asyncAwait.js
+++ asyncAwait.js
@@ -1,17 +1,16 @@
-function fetchPersons() {
+function fetchDragons() {
     return fetch('https://api.spacexdata.com/v3/dragons')
 }
 
 async function loadData() {
     let fetchResult, parsedResponse;
     try {
-        fetchResult = await fetchPersons()
+        fetchResult = await fetchDragons()
         if (!fetchResult.ok) {
             throw new Error(fetchResult.statusText)
         }
         parsedResponse = await fetchResult.json();
         console.log(parsedResponse);
-        return parsedResponse;
     } catch (err) {
         console.error(err)
     }
